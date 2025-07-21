4cf64e441a11bfeac1cb7aa4bccedbac858c2730
Rahul <rshewatkar@gmail.com>
2025-07-21 21:20:27 +0530

    Vehical price prediction project - code updated and final model save with joblib file

--- Vehicle Price Prediction/data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np  
-
-file_path = r"D:\ML\Projects-20240722T093004Z-001\Projects\vehicle_price_prediction\Vehicle Price Prediction\dataset.csv"
-vehical_data = pd.read_csv(file_path)   
-vehical_data.head()  # Display the first few rows of the dataset
-
-df = pd.DataFrame(vehical_data)
-
